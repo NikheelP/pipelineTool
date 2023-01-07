34eeff37d5ebb599ca0a37e71d7a31be1720827e
Admin <p.nikheel@gmail.com>
2023-01-06 17:44:49 -0800

    TestingOne

---
+++ test.py
@@ -0,0 +1,24 @@
+data = {
+    "Hey": 123456,
+    "sss": 12222,
+    "Hey": 123456,
+    "sss": 12222,
+    "Hey": 123456,
+    "sss": 12222,
+    "Hey": 123456,
+    "sss": 12222,
+    "Hey": 123456,
+    "sss": 12222,
+    "Hey": 123456,
+    "sss": 12222,
+    "Hey": 123456,
+    "sss": 12222,
+}
+
+print('this is test')
+print('another thing is working now')
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+
